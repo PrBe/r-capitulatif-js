c51ac918c31030b84d248ce5cd44153cbb90e12a
akin <akin91.ay@gmail.com>
2021-10-18 10:18:16 +0200

    [EDIT] Correction Akin

--- event.js
+++ event.js
@@ -1,16 +1,20 @@
 
-//Akin - changement de couleur des que l'on clique sur une des div, recliquer remet la couleur d'origine//
-var b1 = document.getElementById("b1")
+//Akin - changement de couleur des que l'on clique sur une des div, recliquer remet la couleur d'origine + changement de label des que l'on clique sur les différents div "Cliquer ici -> Vert"//
+var b1 = document.getElementById("b1");
+var b2 = document.getElementById("b2");
+var b3 = document.getElementById("b3");
+var b4 = document.getElementById("b4");
+
 
 b1.onclick = function (e) {
     
     if(b1.style.backgroundColor=="green"){
         b1.style.background="gray";
-
-       
+        document.getElementById('vert').innerHTML = 'Clique ici'; 
     }
     else{
         b1.style.background="green";
+        document.getElementById('vert').innerHTML = 'VERT';
     }
 }
 
@@ -19,10 +23,11 @@ b2.onclick = function (e) {
     
     if(b2.style.backgroundColor=="orange"){
         b2.style.background="gray";
-       
+        document.getElementById('orange').innerHTML = 'Clique ici';
     }
     else{
         b2.style.background="orange";
+        document.getElementById('orange').innerHTML = 'ORANGE';
     }
 }
 
@@ -30,10 +35,11 @@ b3.onclick = function (e) {
     
     if(b3.style.backgroundColor=="red"){
         b3.style.background="gray";
-       
+        document.getElementById('rouge').innerHTML = 'Clique ici';
     }
     else{
         b3.style.background="red";
+        document.getElementById('rouge').innerHTML = 'ROUGE';
     }
 }
 
@@ -42,10 +48,11 @@ b4.onclick = function (e) {
     
     if(b4.style.backgroundColor=="black"){
         b4.style.background="gray";
-       
+        document.getElementById('noir').innerHTML = 'Clique ici'; 
     }
     else{
         b4.style.background="black";
+        document.getElementById('noir').innerHTML = 'NOIR';
     }
 }
 
@@ -67,23 +74,4 @@ document.getElementById("inst").innerHTML = tex1;
 var clicked = document.getElementById("instru-akin").onclick = function(e){
     console.log("instru-akin: " + clicked);
     alert("Instruction : Cliquer sur un rectangle pour lui changer ça couleur, recliquer pour remettre sa couleur d'origine");     
-}
-
-
-// Akin - changement de texte des que l'on clique sur les différents div "Cliquer ici -> Vert"//
-var clicked = document.getElementById("vert").onclick = function(e){
-  document.getElementById('vert').innerHTML = 'VERT';
-}
-
-var clicked = document.getElementById("orange").onclick = function(e){
-  document.getElementById('orange').innerHTML = 'ORANGE';
-}
-
-var clicked = document.getElementById("rouge").onclick = function(e){
-  document.getElementById('rouge').innerHTML = 'ROUGE';
-}
-
-var clicked = document.getElementById("noir").onclick = function(e){
-  document.getElementById('noir').innerHTML = 'NOIR';
-  
 }
