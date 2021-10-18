85bc00f396dc5b5b73e454ce684fa1c9aa958544
Gregor <failyriece@gmail.com>
2021-10-18 09:43:13 +0200

    [ADD] Commentaires

--- event.js
+++ event.js
@@ -10,11 +10,15 @@
 const titleRoll = document.getElementById('TitleRoll');
 const textRoll = document.getElementById('TextRoll');
 
+// Tableau contenant les couleurs du texte
 var colorRoll = [255, 0, 0];
+// Mode de couleur pour connaîtres les modifications à effecteur. 0 est égale à passser à la couleur jaune, donc rajouter du vert au rouge.
 var mode = 0;
 
 function ColorTitleRoll(){
+    // Verification du mode actuel et décide donc de quel couleur est à modifier. Le mode change plus tard.
     if (mode == 0) {
+        // Change la couleur un point par un point afin d'être fluide
         colorRoll[1] ++;
     }else if (mode == 1) {
         colorRoll[0] --
@@ -27,6 +31,7 @@ function ColorTitleRoll(){
     }else {
         colorRoll[2] --
     }
+    // Vérifie si on arrive à une des six possibilités de couleurs max (Rouge/bleu/jaune... complet et donc égale a 255) et change le mode pour faire monter ou baisser une couleur
     if (colorRoll[0] == 255 && colorRoll[1] == 0 && colorRoll[2] == 0) {
         mode = 0;
     }else if (colorRoll[0] ==255 && colorRoll[1] == 255 && colorRoll[2] == 0) {
@@ -40,18 +45,21 @@ function ColorTitleRoll(){
     }else if (colorRoll[0] ==255 && colorRoll[1] == 0 && colorRoll[2] == 255) {
         mode = 5;
     }
-    console.log(mode)
-    let couleur = "rgb("+colorRoll[0]+", "+colorRoll[1]+", "+colorRoll[2]+")"
+    // Ajoute les trois couleurs a la ligne CSS
+    let couleur = "rgb("+colorRoll[0]+", "+colorRoll[1]+", "+colorRoll[2]+")";
+    // Choisis à quoi ajouter la ligne de CSS et lui donne
     titleRoll.style.color = couleur;
 }
+// Lance la fonction ColorTitleRoll tous les 5 millisecondes, permettant de modifier de manière fluide les couleurs
 setInterval(ColorTitleRoll, 5)
 
+// Un grand tableau contenant chacun un tableau pour chacune des 6 couleurs qui s'enchainent
 var colorRollText = [[255,0,0],[255,255,0],[0,255,0],[0,255,255],[0,0,255],[255,0,255]];
+// Les 6 mode de couleurs pour chaque paramètres à rentrer
 var modeText = [0,1,2,3,4,5];
 
-
 function ColorTextRoll(){
-
+    // Boucle pour passer dans chacune de 6 couleurs différentes et les modifiers une par une. Même logique qu'auparavant, mais x6
     for (let Roll = 0; Roll < colorRollText.length; Roll++) {
         if (modeText[Roll] == 0) {
             colorRollText[Roll][1] ++;
@@ -81,6 +89,7 @@ function ColorTextRoll(){
             modeText[Roll] = 5;
         }
     }
+    // Automatise la suuuuuuper longue ligne de couleur à écrire
     let couleurText = "linear-gradient(to right";
     for (let Roll = 0; Roll < colorRollText.length; Roll++) {
         couleurText += ", rgb("+colorRollText[Roll][0]+", "+colorRollText[Roll][1]+", "+colorRollText[Roll][2]+")";
